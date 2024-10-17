f002dc2b3e9a3c2b1e9f13436127cd1219fc9347
drypot <drypot@gmail.com>
2024-10-17 20:41:57 +0900

    update EnvironmentObjectDemo

--- HelloSwiftUI/SwiftUI/Model/EnvironmentObjectDemoView.swift
+++ HelloSwiftUI/SwiftUI/Model/EnvironmentObjectDemoView.swift
@@ -9,48 +9,26 @@ import SwiftUI
 
 struct EnvironmentObjectDemoView: View {
 
-    enum ProductTypes: String, CaseIterable {
-        case product1 = "Product1"
-        case product2 = "Product2"
-        case product3 = "Product3"
+    class UserSettings: ObservableObject {
+        @Published var username: String = "Unknown"
     }
 
-    class AppState: ObservableObject {
-        @Published var productsInStock: Set<ProductTypes> = [.product1, .product2]
-    }
-
-    var appState = AppState()
+    var settings = UserSettings()
 
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(ProductTypes.allCases, id:\.self) { product in
-                    NavigationLink(destination: DetailView(product: product)) {
-                        Text(product.rawValue)
-                    }
-                }
-            }
-            .listStyle(.sidebar)
-
-            Text("Select an item")
-        }
-        .frame(minWidth: 500, minHeight: 300)
-        .environmentObject(appState)
+        SubView()
+            .environmentObject(settings)
     }
 
-    struct DetailView: View {
-        @EnvironmentObject var appState: AppState
-
-        let product: ProductTypes
+    struct SubView: View {
+        @EnvironmentObject var settings: UserSettings
 
         var body: some View {
             VStack {
-                Text(product.rawValue)
-                    .padding()
-                if appState.productsInStock.contains(product) {
-                    Text("In stock")
-                } else {
-                    Text("Out of stock")
+                Text("Username: \(settings.username)")
+
+                Button("Change Username") {
+                    settings.username = "Jane Doe"
                 }
             }
         }
