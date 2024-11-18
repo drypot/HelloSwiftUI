28e33d2d38c1b2ec32d350e66af98fc4ea3cbf16
drypot <drypot@gmail.com>
2024-11-18 12:17:08 +0900

    add AppState

---
+++ HelloSwiftUI/100-App/AppState.swift
@@ -0,0 +1,21 @@
+//
+//  AppState.swift
+//  HelloSwiftUI
+//
+//  Created by Kyuhyun Park on 11/18/24.
+//
+
+import Foundation
+import SwiftUI
+
+// https://developer.apple.com/documentation/combine/observableobject
+// https://developer.apple.com/documentation/observation
+
+// 신형 Observation 으로 만들까 하다가,
+// ObservableObject 과 섞어쓰면 헷갈릴테니,
+// 그리고 아무래도 Combine 도 재미로 다시 한번 써봐야 하니,
+// 그냥 ObservableObject 를 사용하는 것으로.
+
+class AppState: ObservableObject {
+
+}
--- HelloSwiftUI/100-App/HelloSwiftUIApp.swift
+++ HelloSwiftUI/100-App/HelloSwiftUIApp.swift
@@ -7,10 +7,21 @@
 
 import SwiftUI
 
+// https://developer.apple.com/documentation/swiftui/app
+
+// App 프로토콜을 구현하는 스트럭쳐가 app 실행의 진입점이 된다.
+
 @main
 struct HelloSwiftUIApp: App {
 
+    // Scene 의 라이프 사이클과 관계없이 계속 보존해야 하는 state 들은 App 에 정의한다.
+    
+    @StateObject var appState = AppState()
+
     var body: some Scene {
+
+        // 아랙 각종 Scene 인스턴스들은 View 트리의 루트를 갖는다.
+
         WindowGroup("SwiftUI Demo") {
             DemoNavigator()
                 .useDisplayMode()
