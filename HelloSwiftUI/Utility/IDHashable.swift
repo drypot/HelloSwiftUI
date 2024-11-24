3f5973121a6a44604d95a7d46d0b33d6a3a359f4
drypot <drypot@gmail.com>
2024-11-24 21:51:34 +0900

    add IDHashable

---
+++ HelloSwiftUI/Utility/IDHashable.swift
@@ -0,0 +1,21 @@
+//
+//  IDHashable.swift
+//  HelloSwiftUI
+//
+//  Created by Kyuhyun Park on 11/24/24.
+//
+
+import Foundation
+
+protocol IDHashable: Identifiable, Hashable {
+}
+
+extension IDHashable {
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        return lhs.id == rhs.id
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
