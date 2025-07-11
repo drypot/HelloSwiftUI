886ec37f1910807f3e878bdf690d1a034105ca0c
drypot <drypot@gmail.com>
2025-07-11 14:14:33 +0900

    update ObservableDemo

--- HelloSwiftUI/110-State/ObservableDemo.swift
+++ HelloSwiftUI/110-State/ObservableDemo.swift
@@ -9,33 +9,33 @@ import SwiftUI
 
 // https://developer.apple.com/documentation/swiftui/state
 
-@Observable fileprivate class GlobalModel {
-    var counter = 0
-
-    @MainActor static let shared = GlobalModel()
-}
-
 struct ObservableDemo: View {
 
-    // ObservableObject는 전체 객체에 대해 View가 갱신된다.
-    // Observable은 변경된 속성만 추적하여 더 정밀한 View 업데이트 가능.
-
-    @Observable class Model {
+    @Observable
+    @MainActor
+    class Model {
         var counter = 0
+
+        static let shared = Model()
     }
+
+    // ObservableObject는 오브젝트 단위에서 변경 알림이 발생한다.
+    // Observable은 프로퍼티 단위에서 변경 알림이 발생한다.
+    // 더 효율적인 View 갱신이 가능.
 
     // @State 들은 private 으로 선언한다.
     // private 이 없어서 실수로 memberwise initializer 를 넣으면
     // 뷰 생성마다 State 가 초기화된다.
 
+    // View 를 날렸다 다시 만들면 @State data 가 초기화 됨을 확인하기 위해 isPresented 를 쓴다.
+
     @State private var isPresented = true
     @State private var envModel = Model()
 
     var body: some View {
-        VStack {
-
-            // View 를 날렸다 다시 만들면 @State data 가 초기화 됨을 확인하기 위한 절차.
-            // 실제 State 데모는 아래 BaseView 부터 시작된다.
+        Form {
+            Text("Observable Demo")
+                .font(.title)
 
             Button("Toggle View") {
                 isPresented.toggle()
@@ -43,93 +43,133 @@ struct ObservableDemo: View {
             .padding()
 
             if isPresented {
-                BaseView()
+                ParentView()
                     .environment(envModel)
             } else {
                 Spacer()
             }
         }
+        .formStyle(.grouped)
     }
 
-    struct BaseView: View {
+    struct ParentView: View {
         @State private var counter = 0
-
         // @Observable 상태관리에는 @StateObject 대신 걍 @State 를 쓴다.
         @State private var model = Model()
+        @Environment(Model.self) private var envModel
+        private var globalModel = Model.shared
 
         var body: some View {
-            Form {
-                Text("Observable Demo")
-                    .font(.title)
-
-                ReadOnlySubView(counter: counter, model: model)
-
-                // @Bindable 에 넘길 때 $ 를 붙이지 않는다.
-                ReadWriteSubView(counter: $counter, model: model)
+            Section {
+                Text("Base View")
+                    .font(.headline)
+                    .padding(4)
+                LabeledContent("local counter") {
+                    Stepper(value: $counter) {
+                        Text("\(counter)")
+                    }
+                }
+                LabeledContent("local model counter") {
+                    Stepper(value: $model.counter) {
+                        Text("\(model.counter)")
+                    }
+                }
+                // @Environment 는 $ 프로젝션을 제공하지 않아서 Binding이 필요하면 수동으로 만들어줘야 한다.
+                LabeledContent("environment model counter") {
+                    let binding = Binding(
+                        get: { envModel.counter },
+                        set: { envModel.counter = $0 }
+                    )
+                    Stepper(value: binding) {
+                        Text("\(envModel.counter)")
+                    }
+                }
+                LabeledContent("global model counter") {
+                    let binding = Binding(
+                        get: { globalModel.counter },
+                        set: { globalModel.counter = $0 }
+                    )
+                    Stepper(value: binding) {
+                        Text("\(globalModel.counter)")
+                    }
+                }
             }
-            .formStyle(.grouped)
+
+            ReadOnlySubView(counter: counter, model: model)
+
+            // @Bindable 에 넘길 때 $ 를 붙이지 않는다.
+            ReadWriteSubView(counter: $counter, model: model)
         }
+
     }
 
     struct ReadOnlySubView: View {
         var counter: Int
-
         let model: Model
-
         @Environment(Model.self) private var envModel
+        private let globalModel = Model.shared
 
         var body: some View {
             Section {
                 Text("ReadOnly SubView")
                     .font(.headline)
                     .padding(4)
-                LabeledContent("counter") {
+                LabeledContent("parent counter") {
                     Text("\(counter)")
                 }
-                LabeledContent("model.counter") {
+                LabeledContent("parent model counter") {
                     Text("\(model.counter)")
                 }
-                LabeledContent("environment model.counter") {
+                LabeledContent("environment model counter") {
                     Text("\(envModel.counter)")
                 }
-                LabeledContent("global model.counter") {
-                    Text("\(GlobalModel.shared.counter)")
+                LabeledContent("global model counter") {
+                    Text("\(globalModel.counter)")
                 }
             }
         }
     }
 
     struct ReadWriteSubView: View {
+        // @Observable 이 아닌 @State 에 대한 바인딩이 필요하면 @Binging 으로 받는다.
         @Binding var counter: Int
-
-        // 서브 뷰로 바인딩이 필요하면 @Binging 대신 @Bindable 을 쓴다.
+        // @Observable @State 에 대한 바인딩이 필요하면 @Binging 대신 @Bindable 로 받는다.
         @Bindable var model: Model
-        
         @Environment(Model.self) private var envModel
+        private let globalModel = Model.shared
 
         var body: some View {
             Section {
                 Text("ReadWrite SubView")
                     .font(.headline)
                     .padding(4)
-                LabeledContent("counter") {
-                    Button("Increment") {
-                        counter += 1
+                LabeledContent("parent counter") {
+                    Stepper(value: $counter) {
+                        Text("\(counter)")
                     }
                 }
-                LabeledContent("model.counter") {
-                    Button("Increment") {
-                        model.counter += 1
+                LabeledContent("parent model counter") {
+                    Stepper(value: $model.counter) {
+                        Text("\(model.counter)")
                     }
                 }
-                LabeledContent("environment model.counter") {
-                    Button("Increment") {
-                        envModel.counter += 1
+                // @Environment 는 $ 프로젝션을 제공하지 않아서 Binding이 필요하면 수동으로 만들어줘야 한다.
+                LabeledContent("environment model counter") {
+                    let binding = Binding(
+                        get: { envModel.counter },
+                        set: { envModel.counter = $0 }
+                    )
+                    Stepper(value: binding) {
+                        Text("\(envModel.counter)")
                     }
                 }
-                LabeledContent("global model.counter") {
-                    Button("Increment") {
-                        GlobalModel.shared.counter += 1
+                LabeledContent("global model counter") {
+                    let binding = Binding(
+                        get: { globalModel.counter },
+                        set: { globalModel.counter = $0 }
+                    )
+                    Stepper(value: binding) {
+                        Text("\(globalModel.counter)")
                     }
                 }
             }
