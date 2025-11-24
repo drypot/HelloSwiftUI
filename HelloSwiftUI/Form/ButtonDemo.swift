b070ffd282a51dace36b820ebe2de617a484734d
drypot <drypot@gmail.com>
2025-11-24 18:46:51 +0900

    update ButtonDemo

--- HelloSwiftUI/Form/ButtonDemo.swift
+++ HelloSwiftUI/Form/ButtonDemo.swift
@@ -9,99 +9,92 @@ import SwiftUI
 
 struct ButtonDemo: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Labeled Buttons")
-                .font(.title)
+        ScrollView {
+            Form {
+                Section("Labeled Buttons") {
+                    Button("Button") {
+                        print("clicked")
+                    }
 
-            Button("Button") {
-                print("clicked")
-            }
+                    Button("SystemImage", systemImage: "2.square.fill") {
+                        print("clicked")
+                    }
 
-            Button("Button with SystemImage", systemImage: "2.square.fill") {
-                print("clicked")
-            }
+                    Button {
+                        print("clicked")
+                    } label: {
+                        Text("Label")
+                    }
 
-            Button {
-                print("clicked")
-            } label: {
-                Text("Button with Label")
-            }
+                    Button {
+                        print("clicked")
+                    } label: {
+                        Label("Label and SystemImage", systemImage: "2.square.fill")
+                    }
 
-            Button {
-                print("clicked")
-            } label: {
-                Label("Button with Label and Image", systemImage: "2.circle")
-            }
+                    Button {
+                        print("clicked")
+                    } label: {
+                        HStack {
+                            Text("HStack")
+                            Image(systemName: "square.and.arrow.down.fill")
+                        }
+                    }
+                }
 
-            Button {
-                print("clicked")
-            } label: {
-                HStack {
-                    Text("Button HStack")
-                    Image(systemName: "square.and.arrow.down.fill")
+                Section("Styled Buttons") {
+                    Button ("Unstyled") {
+                        print("clicked")
+                    }
+
+                    Button ("Plain") {
+                        print("clicked")
+                    }
+                    .buttonStyle(.plain)
+
+                    Button ("Automatic") {
+                        print("clicked")
+                    }
+                    .buttonStyle(.automatic)
+
+                    Button ("Bordered") {
+                        print("clicked")
+                    }
+                    .buttonStyle(.bordered)
+
+                    Button ("Borderless") {
+                        print("clicked")
+                    }
+                    .buttonStyle(.borderless)
+
+                    Button ("BorderedProminent") {
+                        print("clicked")
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
+
+                Section("Modified Buttons") {
+                    Button ("Modified") {
+                        print("clicked")
+                    }
+                    .frame(width: 200, height: 40)
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    .cornerRadius(10)
+                    .buttonStyle(.plain)
+
+                    Button("Modified") {
+                        print("clicked")
+                    }
+                    .tint(.green)
+                    .buttonStyle(.bordered)
+                    .buttonBorderShape(.capsule)
+                    .controlSize(.large)
                 }
             }
-
-            Spacer()
-                .frame(height: 32)
-
-            Text("Styled Buttons")
-                .font(.title)
-
-            Button ("Unstyled") {
-                print("clicked")
-            }
-
-            Button ("Plain") {
-                print("clicked")
-            }
-            .buttonStyle(.plain)
-
-            Button ("Automatic") {
-                print("clicked")
-            }
-            .buttonStyle(.automatic)
-
-            Button ("Bordered") {
-                print("clicked")
-            }
-            .buttonStyle(.bordered)
-
-            Button ("Borderless") {
-                print("clicked")
-            }
-            .buttonStyle(.borderless)
-
-            Button ("BorderedProminent") {
-                print("clicked")
-            }
-            .buttonStyle(.borderedProminent)
-
-            Spacer()
-                .frame(height: 32)
-
-            Text("Modified Buttons")
-                .font(.title)
-
-            Button ("Button Modified") {
-                print("clicked")
-            }
-            .frame(width: 200, height: 40)
-            .font(.title)
-            .foregroundColor(.white)
-            .background(.blue)
-            .cornerRadius(10)
-            .buttonStyle(.plain)
-
-            Button("Button Modified") {
-                print("clicked")
-            }
-            .tint(.green)
-            .buttonStyle(.bordered)
-            .buttonBorderShape(.capsule)
-            .controlSize(.large)
+            .formStyle(.grouped)
         }
-        .padding()
     }
 }
 
