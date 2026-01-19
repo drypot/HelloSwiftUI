dfb21064516496159e46009c30d2806547f57344
drypot <drypot@gmail.com>
2026-01-19 23:33:49 +0900

    updaate TaskDemo

--- HelloSwiftUI/Concurrency/TaskDemo.swift
+++ HelloSwiftUI/Concurrency/TaskDemo.swift
@@ -24,29 +24,63 @@ struct TaskDemo: View {
 
             Text("Counter: \(counter)")
 
-            Button("Call task") {
+            Button("Call task fetchData") {
+
+                // closure 들은 Actor 를 계승한다, View 의 MainActor 가 계승된다.
+
                 Task {
+
+                    // Task 도 Actor 를 계승한다, View 의 MainActor 가 계승된다.
+
                     await fetchData()
                     counter += 1
                 }
             }
 
-            Button("Call detached task") {
+            Button("Call task fetchDataConcurrent") {
+                Task {
+                    // Task.detached 가 있기는 한데,
+                    // Task.detached 대신,
+                    // async 펑션에 @concurrent 붙이는 것이 원만하다.
+                    await fetchDataConcurrent()
+                    counter += 1
+                }
+            }
+
+            Button("Call detached fetchData") {
                 Task.detached {
+
+                    // No actor isolation, runs on cooperative pool
+
+                    await fetchData()
+                    await MainActor.run {
+                        counter += 1
+                    }
+                }
+            }
+
+            Button("Call detached fetchDataConcurrent") {
+                Task.detached {
+
+                    // No actor isolation, runs on cooperative pool
+
                     await fetchDataConcurrent()
                     await MainActor.run {
                         counter += 1
                     }
                 }
             }
+
         }
     }
 
+    // @MainActor 가 기본으로 붙으니 MainActor 에서 실행된다.
     func fetchData() async {
         assert(Thread.isMainThread)
         print("fetchData: ...")
     }
 
+    // @concurrent 가 붙으면 Actor 바깥에서 실행된다.
     @concurrent func fetchDataConcurrent() async {
         assert(!Thread.isMainThread)
         print("fetchDataConcurrent: ...")
