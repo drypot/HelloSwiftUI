c0dadac5942083ac2d0906cb0ef3079e3c265331
drypot <drypot@gmail.com>
2026-01-25 23:32:59 +0900

    update ViewIDDemo

--- HelloSwiftUI/State/ViewIDDemo.swift
+++ HelloSwiftUI/State/ViewIDDemo.swift
@@ -9,32 +9,40 @@ import SwiftUI
 
 struct ViewIDDemo: View {
 
-    @State var value = 0
+    @State var value1 = 10
+    @State var value2 = 10
 
     var body: some View {
         Form {
 
-            // SubView 에 넘기는 인자 value 를 업데이트해도 SubView 인스턴스가 새로 만들어 지지만
-            // 새로운 id 를 넣어도 새로 만들어 진다.
+            // View는 고유 식별자와 연결해서 State 저장소를 관리한다.
+            // 첫 SubView가 만들어질 때 State도 만들어지고 그 이후에는 새로 만들어지지 않는다.
+            // SubView init 의 value 인자에 새로운 값을 전달해도 State 는 새로 만들어지지 않는다.
+            // 정 새로 만들고 싶으면 뷰의 id 를 변경해야 한다.
 
-            SubView(value: 10).id(value)
-            Button("Increase Value") {
-                value += 1
+            SubView(value: value1).id(value2)
+            Button("Increase Value1") {
+                value1 += 1
+                print("value1: \(value1), value2: \(value2)")
+            }
+            Button("Increase Value2") {
+                value2 += 1
+                print("value1: \(value1), value2: \(value2)")
             }
         }
         .formStyle(.grouped)
     }
 
     struct SubView: View {
-        let value: Int
+        @State var value: Int
 
         init(value: Int) {
             self.value = value
-            print("Sub view created: \(value)")
+            print("SubView created: \(value), \(self.value)")
         }
 
         var body: some View {
-            Text("Sub View: \(value)")
+            Text("SubView: \(value)")
         }
     }
 }
